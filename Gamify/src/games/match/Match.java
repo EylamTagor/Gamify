e78eb4a9472c2787aad6daaf588e892ce2966e88
Eylam Tagor <eylamtagor@gmail.com>
2018-10-21 00:44:10 -0700

    Fix last bug(?) in Match minigame

--- Gamify/src/games/match/Match.java
+++ Gamify/src/games/match/Match.java
@@ -10,8 +10,8 @@ import processing.core.PApplet;
 public class Match {
 
 	public static void main(String args[]) {
-		MatchDrawingSurface drawing = new MatchDrawingSurface(args[0]);
-//		MatchDrawingSurface drawing = new MatchDrawingSurface("studysettest.txt");
+//		MatchDrawingSurface drawing = new MatchDrawingSurface(args[0]);
+		MatchDrawingSurface drawing = new MatchDrawingSurface("studysettest.txt");
 		PApplet.runSketch(new String[] { "" }, drawing);
 		PSurfaceAWT surf = (PSurfaceAWT) drawing.getSurface();
 		PSurfaceAWT.SmoothCanvas canvas = (PSurfaceAWT.SmoothCanvas) surf.getNative();
@@ -22,6 +22,9 @@ public class Match {
 		window.setMinimumSize(new Dimension(100, 100));
 		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		window.setResizable(true);
+
+		window.setLocation(100, 100);
+		window.setSize(800, 600);
 
 		window.setVisible(true);
 		canvas.requestFocus();
